6db56efa96e9a9ee34e20ac196c9da199de8feb8
fiona <13773907@qq.com>
2017-07-06 16:28:49 +0800

    implement part 2

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,9 @@ Rails.application.routes.draw do
 
    resources :jobs
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+   namespace :admin do
+     resources :jobs
+   end
    root 'welcome#index'
 
 end
