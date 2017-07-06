137b87850b7ac451e89b330addbbd729fa418d15
fiona <13773907@qq.com>
2017-07-07 01:10:27 +0800

    implement step 5

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,14 @@
 Rails.application.routes.draw do
   devise_for :users
 
-   resources :jobs
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
    namespace :admin do
-     resources :jobs
+     resources :jobs do
+       member do
+         post :pulish
+         post :hide
+       end
+     end
    end
    root 'welcome#index'
 
